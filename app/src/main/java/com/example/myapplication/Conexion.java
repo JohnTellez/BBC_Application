117a0f6b619d91203f6edfe30ad514579af13237
John Tellez <jtellezm1976@universidadean.edu.co>
2019-04-15 09:06:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Conexion.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class Conexion {
+
+    public static final String URL_WEB_SERVICE = "http://192.168.119.237/NavigationDrawer/";
+}
---
+++ app/src/main/java/com/example/myapplication/Inicio.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Inicio extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+    }
+
+    public void onClick(View view){
+
+        Intent miIntent = new Intent( Inicio.this, Login.class);
+        startActivity(miIntent);
+
+    }
+}
